5b3c0bf60d07af1a3c367fec0a11b7651bff3e7b
BenjaminCCG <42145150+BenjaminCCG@users.noreply.github.com>
2023-08-02 10:25:16 +0800

    feat(components                     (组件相关)): submit

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -2,9 +2,14 @@
   <div>{{ userStore.name }}</div>
   <span>asdas</span>
   {{ abc }} {{ b }}
+  {{ props.name }}
 </template>
 
 <script setup lang="ts">
+const props = defineProps<{
+  name: string;
+  age: number;
+}>();
 import useUserStore from '@/store/user.ts';
 import { ref } from 'vue';
 const userStore = useUserStore();
